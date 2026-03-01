1b0cf55a29f0750ab6c5f73f95646152cb80942b
phoenix2429 <loghamithra345@gmail.com>
2026-03-01 10:00:10 +0530

    added another user

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: "20px", background: "#111", color: "white" }}>
+    <nav style={{ padding: "20px", background: "#4f1212", color: "#fbc9ba" }}>
       <Link to="/" style={{ marginRight: "20px", color: "white" }}>
         Dashboard
       </Link>
@@ -10,8 +10,8 @@ export default function Navbar() {
         Leaderboard
       </Link>
       <Link to="/analytics" style={{ marginLeft: "20px", color: "white" }}>
-  Analytics
-</Link>
+        Analytics
+      </Link>
     </nav>
   );
 }
